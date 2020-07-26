fcd92a8ede9734a94d559ce4b322d350e03b68ce
Barrett Langton <barrett@rehabcreative.com>
2020-07-26 15:02:06 -0600

    Set ag to not follow symbolic links by default

--- zsh/configs/search.zsh
+++ zsh/configs/search.zsh
@@ -1,5 +1,5 @@
 # FZF default command.
-export FZF_DEFAULT_COMMAND='ag -U -f --column --hidden -W 120 -S -g ""'
+export FZF_DEFAULT_COMMAND='ag -U --column --hidden -W 120 -S -g ""'
 
 # FZF default options (use exact matches by default).
 export FZF_DEFAULT_OPTS='--exact'
