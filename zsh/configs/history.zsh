411ba968516fe3954cba2593207afe5c9d5dde53
Barrett Langton <barrett@langton.dev>
2020-09-22 06:00:51 -0600

    Add more history customization to zsh

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -4,3 +4,12 @@ export SAVEHIST=10000
 
 # Ignore duplicates.
 setopt hist_ignore_all_dups
+
+# Share history across multiple zsh sessions.
+setopt share_history
+
+# Append to history.
+setopt append_history
+
+# Remove blank lines from history.
+setopt hist_reduce_blanks
