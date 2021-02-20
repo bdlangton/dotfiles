2e690c176f4635fbbd0e77d5bba9b54a7491926b
Barrett Langton <blangton@hubtran.com>
2021-02-20 14:52:14 -0700

    Add fzf history to fzf commands (shell & vim)
    This lets you easily input the same search to fzf again. Once you pull
    up fzf, you can ctrl-p to cycle through previous searches.

--- zsh/configs/search.zsh
+++ zsh/configs/search.zsh
@@ -2,6 +2,6 @@
 export FZF_DEFAULT_COMMAND='ag -U --column --hidden -W 120 -S -g ""'
 
 # FZF default options (use exact matches by default).
-export FZF_DEFAULT_OPTS='--exact'
+export FZF_DEFAULT_OPTS="--exact --history=$HOME/.fzf_history"
 
 # vim:ft=sh
